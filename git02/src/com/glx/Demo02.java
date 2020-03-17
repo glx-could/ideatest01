1ca7d62ead38431da450facaf488c8fd49abcd3d
gaolanxiang <1324868773@qq.com>
2020-03-18 02:12:54 +0800

    第一次修改的代码提交

--- git02/src/com/glx/Demo02.java
+++ git02/src/com/glx/Demo02.java
@@ -4,4 +4,9 @@ public class Demo02 {
     public static void main(String[] args) {
         System.out.println("2222222");
     }
+
+
+    public void test02(){
+        System.out.println("第一次修改代码");
+    }
 }
